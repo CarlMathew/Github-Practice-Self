37baf048e76ab48a49949d1bb03dbdbde889d7cb
Carlito <CarlMathew.Morada@sgs.com>
2026-04-15 11:54:35 +0800

    add multiplacation function

--- domain/calculator.py
+++ domain/calculator.py
@@ -4,3 +4,8 @@ def add(a: int, b: int) -> int:
 
     add_calculation = a + b
     return add_calculation
+
+
+def multiplaction(a: int, b:int) -> int:
+    multiplaction_calculation = a * b
+    return multiplaction
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,9 @@
-from domain.calculator import add
+from domain.calculator import add, multiplaction
 
 
 def test_add():
     assert add(5, 2) == 7
+
+
+def test_multiplaction():
+    assert multiplaction(5 * 5) == 25
